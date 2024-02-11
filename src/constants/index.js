67a7ee17d6fb93a5d6d2bdcd295be694d863a08f
NataliaOrlova <natakarelskaya@yandex.ru>
2024-02-11 15:17:29 +0400

    added .env

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,2 @@
-export const API_KEY = '8ff819f2e686695a60cb44067ee6149b'
+export const API_KEY = import.meta.env.VITE_API_KEY
 export const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather'
